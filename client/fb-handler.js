09a1084166901789cbf48db571cb39c5391d31b7
MKS-Student <mks.student@hackreactor.com>
2016-11-29 17:17:47 -0800

    Add express serve static files

---
+++ client/fb-handler.js
@@ -0,0 +1,63 @@
+console.log('inside fb-handler.js');
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk/debug.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '199213737151733',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v2.8'
+  });
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+};
+
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    queryAPI();
+  } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  } else {
+    // The person is not logged into Facebook, so we're not sure if
+    // they are logged into this app or not.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
+  }
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function queryAPI() {
+console.log('Welcome!  Fetching your information.... ');
+FB.api('/me/feed', 'GET', {"with":"location","fields":"id,name,place"}, function(response) {
+  console.log('Successful login for: ' + response.name);
+  document.getElementById('status').innerHTML =
+    'Your response is: ' + response;
+  console.log('response is ', response);
+  });
+
+response.paging
+}
--- server/server.js
+++ server/server.js
@@ -1,30 +1,11 @@
 var express = require('express');
-// var fs = require('fs');
 var app = express();
-// var router = express.Router();
 
 app.listen(4568);
 console.log('Listening on port 4568');
 
-app.get('/fbnew', function(req, res) {
-    res.sendFile(__dirname + '/fb.html', function (err) {
-      if (err) {
-        console.log('err is ', err);
-      }
-    });
-});
+app.use(express.static('client'));
 
-app.get('/fbold', function(req, res) {
-    res.sendFile(__dirname + '/fb2.html', function (err) {
-      if (err) {
-        console.log('err is ', err);
-      }
-    });
+app.get('/fb', function(req, res) {
+    res.redirect('/fb.html');
 });
-/*
-app.get('/test', function(req, res) {
-  console.log('reading!');
-    res.redirect('http://www.google.com');
-});
-
-*/
