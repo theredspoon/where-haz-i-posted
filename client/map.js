db41b6c437c6d6e308a7d4748af926f78c690a77
MKS-Student <mks.student@hackreactor.com>
2016-11-29 19:56:11 -0800

    Add Google Maps MarkerClusterer library and display clusters on map

--- client/map.js
+++ client/map.js
@@ -4,7 +4,7 @@ var map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.0625, lng: -95.677068},
+    center: {lat: 37.16031655, lng: -61.5234375},
       zoom: 2
   });
 
@@ -12,15 +12,19 @@ function initMap() {
 };
 
 window.eqfeed_callback = function (latLngArray) {
-
+  let markers = [];
   for (let i = 0, l = latLngArray.length; i < l; i++) {
-    console.log('latLngArray[i].lng is ', latLngArray[i].lng);
     let latLng = new google.maps.LatLng(latLngArray[i].lat,latLngArray[i].lng);
-    console.log('latLng is ', latLng);
 
     let marker = new google.maps.Marker({
       position: latLng,
       map: map
     });
+
+    markers.push(marker);
   }
+
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath: './assets/map/m'
+  });
 }
--- client/utils.js
+++ client/utils.js
@@ -30,16 +30,30 @@ DATA IS AN ARRAY OF POST OBJECTS
 //response:location: latitude, longitude
 // parse FB data and output array of lat/long objects
 var getFBData = function (response) {
-  let output = [];
+  console.log('in getFBData');
+  let output = [
+    {lat: 52.518611, lng: 13.408056}, 
+    {lat: 51.519475, lng: 7.46694444},
+    {lat: 51.50, lng: 7.40}, 
+    {lat: 51.555, lng: 7.40},
+    {lat: 51.555, lng: 7.625},
+    {lat: 51.5125, lng: 7.625},
+    {lat: 51.5103, lng: 7.49347},
+    {lat: 52.516272, lng: 13.377722},
+    {lat: 51.515, lng: 7.453619},
+    {lat: 51.503333, lng: -0.119722},
+    {lat: 52.516272, lng: 13.377722},
+    {lat: 51.515, lng: 7.453619},
+    {lat: 51.503333, lng: -0.119722},
+    {lat: 55.751667, lng: 37.617778},
+    {lat: 48.8583, lng: 2.2945},
+    {lat: 59.3275, lng: 18.0675},
+    {lat: 59.916911, lng: 10.727567}
+    ];
   for (let post of response.data) {
     output.push({lat: post.place.location.latitude, lng: post.place.location.longitude});
   }
   console.log('output is ', output);
   return output;
-
 }
 
-// take an array of lat/long objects and create markers on GMaps
-var createGoogleMapsMarker = function (array) {
-
-}
